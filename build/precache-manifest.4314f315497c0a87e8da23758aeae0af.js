acb2a7843f11263b08aa57ce8c6f4bc769bd8069
Luis A <zeronarratore@gmail.com>
2019-04-30 17:04:36 -0500

    deploy

---
+++ build/precache-manifest.4314f315497c0a87e8da23758aeae0af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e93e907cb951b758c7d4",
+    "url": "/static/js/main.143804f3.chunk.js"
+  },
+  {
+    "revision": "f7a8160bf2e8bc14f3bf",
+    "url": "/static/js/2.a48fc874.chunk.js"
+  },
+  {
+    "revision": "e93e907cb951b758c7d4",
+    "url": "/static/css/main.41bf526d.chunk.css"
+  },
+  {
+    "revision": "f7a8160bf2e8bc14f3bf",
+    "url": "/static/css/2.6fdca5f7.chunk.css"
+  },
+  {
+    "revision": "1d8e461ea3b38a363942a4bb80511f6e",
+    "url": "/index.html"
+  }
+];
