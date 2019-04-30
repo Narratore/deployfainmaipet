59f710f701f0c41ad8e9aeb38b23762e0b5628df
Luis A <zeronarratore@gmail.com>
2019-04-30 17:38:36 -0500

    adding new build

---
+++ build/precache-manifest.8744b1e26132a5db4f056f5e92ec6ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d1e7c3547c3820b5ca1",
+    "url": "/static/js/main.8bff7352.chunk.js"
+  },
+  {
+    "revision": "e762614d31e5b31563a8",
+    "url": "/static/js/2.a48fc874.chunk.js"
+  },
+  {
+    "revision": "6d1e7c3547c3820b5ca1",
+    "url": "/static/css/main.cf91eecd.chunk.css"
+  },
+  {
+    "revision": "e762614d31e5b31563a8",
+    "url": "/static/css/2.17c8f2ad.chunk.css"
+  },
+  {
+    "revision": "13871b8e3d28c1230a7588236d5816c8",
+    "url": "/index.html"
+  }
+];
